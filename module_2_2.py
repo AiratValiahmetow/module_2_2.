3a2b37dfb7aaa606f8ae0d76999a05350400fd54
AiratValiahmetow <valiahmetowairat@yandex.ru>
2024-08-20 22:03:18 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 111
+second = 222
+third = 333
+if first == second and second == third and first == third:
+    print(3)
+if first == second or second == third or first == third:
+        print(2)
+else:
+            print(0)
+
